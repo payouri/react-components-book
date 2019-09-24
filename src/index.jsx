24711ebf9e09f22555dde3b937a279c632c96a14
Pailloux Youri <pailloux.youri@gmail.com>
2019-09-25 01:15:03 +0200

    Update to test Carousel and TimePicker

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,34 @@
 import "regenerator-runtime/runtime";
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Calendar from './components/Calendar/Calendar';
+import TimePicker from 'components/TimePicker/TimePicker';
+import Input from 'components/Input/Input';
+import InputWithDatePicker from 'components/InputWithDatePicker/InputWithDatePicker';
+import Carousel from 'components/Carousel/Carousel';
+import DataFetch from 'hoc/DataFetch';
 
-const Test = Calendar;
+const FetchCarousel =DataFetch(Carousel);
 
-const componentsArray = [
-    Test,
-].map((Component, index) => <Component key={index}/>);
 
+const r = {
+    method: 'Post',
+    url: '/',
+}
+
+// console.log(new Request({...r}));
 class App extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return componentsArray;
+        return (
+            <>
+                <FetchCarousel onResponse={async (res) => { const d = await res.json(); return d }} url={'https://jsonplaceholder.typicode.com/todos/'} />
+                <Input type='number' onChange={(v, valid, e) => console.log(v, valid, e)} />
+                <TimePicker />
+                <InputWithDatePicker />
+            </>
+        )
     }
 }
 
