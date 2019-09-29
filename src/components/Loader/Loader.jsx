8e3f77dc0c177ab005bf8c30456c2a8689f82bbb
Pailloux Youri <pailloux.youri@gmail.com>
2019-09-29 23:24:12 +0200

    add size prop and corresponding styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,8 +6,8 @@ import styles from './Loader.scss';
 class Loader extends Component {
     render() {
         return (
-            <div className={styles['loader-outer']}>
-                <div className={styles['loader']} style={{
+            <div className={styles['loader-outer']} style={this.props.style}>
+                <div className={`${styles['loader']} ${styles[`loader-${this.props.size}`]}`} style={{
                     background: this.props.background,
                     borderColor: this.props.color,
                 }}></div>
@@ -18,7 +18,11 @@ class Loader extends Component {
 Loader.propTypes = {
     background: PropTypes.string,
     color: PropTypes.string,
+    style: PropTypes.object,
+    size: PropTypes.oneOf(['sm', 'md', 'lg']).isRequired,
 }
-
+Loader.defaultProps = {
+    size: 'md'
+}
 
 export default Loader;
