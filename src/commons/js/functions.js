ce83af0d9b21faf4e0a1cf1d91514b5779ecf418
Pailloux Youri <pailloux.youri@gmail.com>
2019-09-29 23:23:28 +0200

    add getElementAt

--- src/commons/js/functions.js
+++ src/commons/js/functions.js
@@ -2,6 +2,14 @@ export const clamp = function(number, min, max) {
     return number > max ? max : number < min ? min : number;
 }
 
+export const getElementAt = function(iterable, n) {
+    if(iterable.length == 0) {
+        return undefined;
+    }
+    const i = n%iterable.length;
+    return i >= 0 ? iterable[i] : iterable[iterable.length + i];
+}
+
 export const getFirstTouch = function(touchEvent) {
 
     const { touches } = touchEvent;
@@ -15,5 +23,6 @@ export const getFirstTouch = function(touchEvent) {
 
 export default {
     clamp,
+    getElementAt,
     getFirstTouch,
 }
