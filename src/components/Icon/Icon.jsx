8035ab99ef62dba50b713f20c246e710588884cd
Pailloux Youri <pailloux.youri@gmail.com>
2019-11-02 17:01:29 +0000

    add more font awesome icons

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faChevronRight, faChevronUp, faChevronDown, faTrash, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const iconLibrary = library
 
-iconLibrary.add(faChevronLeft, faChevronRight)
+iconLibrary.add(faChevronLeft, faChevronRight, faChevronDown, faChevronUp, faTrash, faTrashAlt)
 
 const Icon = function({ name }) {
     return <FontAwesomeIcon icon={name} />
@@ -15,6 +15,10 @@ Icon.propTypes = {
     name: PropTypes.oneOf([
         'chevron-right',
         'chevron-left',
+        'chevron-up',
+        'chevron-down',
+        'trash',
+        'trash-alt',
     ])
 }
 
