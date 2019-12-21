8c0b73dd1db504f55d84db9741621f5131fc7687
Pailloux Youri <pailloux.youri@gmail.com>
2019-12-21 00:15:43 +0000

    add faTimes

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { faChevronLeft, faChevronRight, faChevronUp, faChevronDown, faTrash, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faChevronRight, faChevronUp, faChevronDown, faTrash, faTrashAlt, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const iconLibrary = library
 
-iconLibrary.add(faChevronLeft, faChevronRight, faChevronDown, faChevronUp, faTrash, faTrashAlt)
+iconLibrary.add(faChevronLeft, faChevronRight, faChevronDown, faChevronUp, faTrash, faTrashAlt, faTimes)
 
-const Icon = function({ name }) {
-    return <FontAwesomeIcon icon={name} />
+const Icon = function({ name, ...rest }) {
+    return <FontAwesomeIcon icon={name} {...rest} />
 }
 Icon.propTypes = {
     name: PropTypes.oneOf([
+        'times',
         'chevron-right',
         'chevron-left',
         'chevron-up',
